d10295549a4b8d393af1578b9f8f387982e08f10
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-10-26 19:36:32 +0530

    Setup publishing to MythicMC Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     java
+    `maven-publish`
     id("com.gradleup.shadow") version "9.2.2"
     id("net.kyori.blossom") version "2.2.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.3" // IntelliJ + Blossom integration
@@ -35,6 +36,8 @@ dependencies {
 }
 
 java {
+    withJavadocJar()
+    withSourcesJar()
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
@@ -50,6 +53,55 @@ sourceSets {
             javaSources {
                 property("version", project.version.toString())
                 property("description", project.description ?: "")
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "mythicmcReleases"
+            url = uri("https://maven.mythicmc.org/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.gmail.tracebachi"
+            artifactId = "deltabans"
+            version = project.version.toString()
+            from(components["java"])
+            pom {
+                name = project.name
+                description = project.description
+                url = "https://github.com/mythicmc/DeltaBans"
+                // properties = mapOf("myProp" to "value", "prop.with.dots" to "anotherValue")
+                licenses {
+                    license {
+                        name = "GPL-3.0-only"
+                        url = "https://spdx.org/licenses/GPL-3.0-only.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "GeeItsZee"
+                        email = "tracebachi@gmail.com"
+                    }
+                    developer {
+                        id = "retrixe"
+                        name = "Ibrahim Ansari"
+                        email = "ibu2@mythicmc.org"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/mythicmc/DeltaBans.git"
+                    developerConnection = "scm:git:ssh://github.com/mythicmc/DeltaBans.git"
+                    url = "https://github.com/mythicmc/DeltaBans/"
+                }
             }
         }
     }
